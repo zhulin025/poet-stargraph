958efea82c86acd913dcf9005c46fef56bc10ae0
zhulin025 <zhulin025@users.noreply.github.com>
2026-03-16 17:52:26 +0800

    fix: 转换配置为 CommonJS 并增加构建日志诊断

--- next.config.js
+++ next.config.js
@@ -4,10 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // 由于部署在 github.io/poet-stargraph/ 下，必须配置路径前缀
   basePath: '/poet-stargraph',
   assetPrefix: '/poet-stargraph/',
   trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
